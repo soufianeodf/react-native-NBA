2ca8b4a5b831af95fc1cf5da26a64c2a469a39ff
soufiane odf <soufiane.odf@gmail.com>
2020-04-19 15:50:44 +0000

    add the auth logo

--- app/Components/auth/index.js
+++ app/Components/auth/index.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
+import AuthLogo from './authLogo';
 
 class AuthComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false,
+    };
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is the auth component.</Text>
-      </View>
-    );
+    if (this.state.loading) {
+      return (
+        <View style={styles.loading}>
+          <ActivityIndicator />
+        </View>
+      );
+    } else {
+      return (
+        <ScrollView style={styles.container}>
+          <View>
+            <AuthLogo />
+          </View>
+        </ScrollView>
+      );
+    }
   }
 } // class
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1d428a',
+    padding: 50,
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
