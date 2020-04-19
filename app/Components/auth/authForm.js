0b0095ac5f1648262f2ae9b9e70394d454cfef05
soufiane odf <soufiane.odf@gmail.com>
2020-04-19 18:57:49 +0000

    finish inputs and error state

--- app/Components/auth/authForm.js
+++ app/Components/auth/authForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 
 import Input from '../../utils/forms/input';
 
@@ -42,6 +42,40 @@ class AuthForm extends React.Component {
     };
   }
 
+  updateInput = (name, value) => {
+    this.setState({
+      hasErrors: false,
+    });
+
+    let formCopy = this.state.form;
+    formCopy[name].value = value;
+
+    this.setState({
+      form: formCopy,
+    });
+  };
+
+  formHasErrors= () => (
+    this.state.hasErrors ?
+        <View style={styles.errorContainer}>
+            <Text style={styles.errorLabel}>Oops, check you info.</Text>
+        </View>
+    : null
+  )
+
+  confirmPassword = () => (
+    this.state.type !== 'Login' ? 
+      <Input
+        placeholder="Confirm your password"
+        placeholderTextColor="#cecece"
+        type={this.state.form.confirmPassword.type}
+        value={this.state.form.confirmPassword.value}
+        onChangeText={value => this.updateInput('confirmPassword', value)}
+        secureTextEntry
+      />
+     : null
+  )
+
   render() {
     return (
       <View>
@@ -52,10 +86,37 @@ class AuthForm extends React.Component {
           type={this.state.form.email.type}
           value={this.state.form.email.value}
           keyboardType={'email-address'}
+          onChangeText={value => this.updateInput('email', value)}
         />
+
+        <Input
+          placeholder="Enter your password"
+          placeholderTextColor="#cecece"
+          type={this.state.form.password.type}
+          value={this.state.form.password.value}
+          onChangeText={value => this.updateInput('password', value)}
+          secureTextEntry
+        />
+
+        {this.confirmPassword()}
+        {this.formHasErrors()}
       </View>
     );
   }
 } // class
+
+const styles = StyleSheet.create({
+  errorContainer: {
+    marginBottom: 10,
+    marginTop: 30,
+    padding: 10,
+    backgroundColor: '#f44336',
+  },
+  errorLabel: {
+    color: '#fff',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+});
 
 export default AuthForm;
