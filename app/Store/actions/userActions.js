8ae976042891998af04f55ff1489c99fec2ffc63
soufiane odf <soufiane.odf@gmail.com>
2020-04-21 14:38:56 +0000

    add the signin logic with firebase

--- app/Store/actions/userActions.js
+++ app/Store/actions/userActions.js
@@ -29,12 +29,29 @@ export function signUp(data) {
   };
 }
 
-export function signIn() {
+export function signIn(data) {
+  const request = axios({
+    method: 'POST',
+    url: SIGNIN,
+    data: {
+      email: data.email,
+      password: data.password,
+      returnSecureToken: true,
+    },
+    header: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(e => {
+      return false;
+    });
+
   return {
     type: SIGN_IN,
-    payload: {
-      email: 'francis@gmail.com',
-      token: 'sldfjqlkjsldkfj',
-    },
+    payload: request,
   };
 }
